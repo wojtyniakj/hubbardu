cbab9a077ad763a6300a144758676bf7134c1f3d
Jakub Wojtyniak <UL0290330@edu.uni.lodz.pl>
2025-10-04 17:51:02 +0200

    Fix max depth of single tree

--- model/model.py
+++ model/model.py
@@ -111,7 +111,7 @@ def train_hubbard_u_model(df):
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) # Zmieniono na 80/20
     
-    model = RandomForestRegressor(n_estimators=5, max_depth=50,random_state=42, n_jobs=-1)
+    model = RandomForestRegressor(n_estimators=100, max_depth=100,random_state=42, n_jobs=-1)
     model.fit(X_train, y_train)
     
     y_pred = model.predict(X_test)
